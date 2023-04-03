dd92a7e161481b2327fd3d373d0efc67b75d80e8
Diego Andrés Alonso Candia <diegod@Latitude-5420-e3c60488.localdomain>
2023-04-02 21:47:44 -0400

    initial commit

---
+++ forward-checking/main.py
@@ -0,0 +1,24 @@
+# FORWARD CHECKING
+
+# add 2 functions: evaluate rows and columns
+
+def search_solution(array, row, rows, cols):
+    new_row = []
+    quantities = rows[row]
+    
+    #while(True):
+
+            
+    
+    
+    if row == 10:
+        return True
+
+def main():
+    cols = [[4], [2], [7], [3, 4], [7, 2], [7, 2], [3, 4], [7], [2], [4]]
+    rows = [[4], [8], [10], [1, 1, 2, 1, 1], [1, 1, 2, 1, 1], [1, 6, 1], [6], [2, 2], [4], [2]]
+
+    search_solution(array=[], col=0, row=0, rows=rows, cols=cols)
+
+if __name__ == '__main__':
+    main()
