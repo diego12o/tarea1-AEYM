d8ccd59300831ec30adf142194e6740686aecd7d
Diego Andrés Alonso Candia <diegod@Latitude-5420-e3c60488.localdomain>
2023-04-08 01:30:40 -0400

    feature: time measurement

--- forward-checking/main.py
+++ forward-checking/main.py
@@ -1,4 +1,5 @@
 import functions
+import time
 
 def main():
     array = [
@@ -13,8 +14,10 @@ def main():
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     ]
-    
+    start = time.time()    
     functions.search_solution(array=array, row=0)
-
+    end = time.time()
+    print("Time: " + str(end-start))
+    
 if __name__ == '__main__':
     main()
